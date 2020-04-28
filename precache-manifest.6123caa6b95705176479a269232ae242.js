37b64f16cebacba19900e13a5a70da03e01db165
damiecode <codenlyn@gmail.com>
2020-04-28 10:04:19 +0100

    Updates

---
+++ precache-manifest.6123caa6b95705176479a269232ae242.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0b86505c517ce3ef671ee25defbbfe0",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "8581d356b70a35931b1b",
+    "url": "/monsters-rolodex/static/css/main.e27c9f1f.chunk.css"
+  },
+  {
+    "revision": "926634218f0cd358d7da",
+    "url": "/monsters-rolodex/static/js/2.a6bff23a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.a6bff23a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8581d356b70a35931b1b",
+    "url": "/monsters-rolodex/static/js/main.cf549a24.chunk.js"
+  },
+  {
+    "revision": "fc1a59712d7f10d18ab0",
+    "url": "/monsters-rolodex/static/js/runtime-main.664c0a0d.js"
+  }
+]);
